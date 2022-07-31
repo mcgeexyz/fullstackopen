a8a0860e746094d04e834ed93e32465070b4c08f
Michael McGee <github@m3t4m1k3.com>
2022-07-31 03:59:31 +0000

    Complete Part 1: Introduction to React: Part A Exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,10 +9,18 @@ const App = () => {
 
   const Header = (props) => <h1>{props.title}</h1>;
 
-  const Content = (props) => (
-    <h2>
+  const Content = () => (
+    <div>
+      <Part description={part1} exerciseCount={exercises1} />
+      <Part description={part2} exerciseCount={exercises2} />
+      <Part description={part3} exerciseCount={exercises3} />
+    </div>
+  );
+
+  const Part = (props) => (
+    <h3>
       {props.description}: {props.exerciseCount} exercises
-    </h2>
+    </h3>
   );
 
   const Total = (props) => <h2>Total number of exercises: {props.total}</h2>;
@@ -20,9 +28,7 @@ const App = () => {
   return (
     <>
       <Header title={course} />
-      <Content description={part1} exerciseCount={exercises1} />
-      <Content description={part2} exerciseCount={exercises2} />
-      <Content description={part3} exerciseCount={exercises3} />
+      <Content />
       <Total total={exercises1 + exercises2 + exercises3} />
     </>
   );
