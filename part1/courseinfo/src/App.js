0daacbe1a9c9daac69eff4fdd0b3cf5409555372
Michael McGee <github@m3t4m1k3.com>
2022-07-31 04:51:27 +0000

    Complete Part 1: Introduction to React: Part A Exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,33 +1,35 @@
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
-  const Header = (props) => <h1>{props.title}</h1>;
+  const Header = (props) => <h1>{props.name}</h1>;
 
   const Content = (props) => (
     <div>
-      <Part content={props.parts[0]} />
-      <Part content={props.parts[1]} />
-      <Part content={props.parts[2]} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 
   const Part = (props) => (
     <h3>
-      {props.content.name}: {props.content.exercises}
+      {props.part.name}: {props.part.exercises}
     </h3>
   );
 
@@ -42,9 +44,9 @@ const App = () => {
 
   return (
     <div>
-      <Header title={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
