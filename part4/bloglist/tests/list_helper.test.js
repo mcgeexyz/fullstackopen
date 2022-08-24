e28815abc8b6f31ce4f8486be41ad23da61ab3c0
Michael McGee <github@m3t4m1k3.com>
2022-08-24 20:25:47 +0930

    Complete part4 exercise 5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,20 +1,19 @@
 const listHelper = require("../utils/list_helper");
 const blogTestData = require("./test_data");
 
-describe("total likes", () => {
-  const blogList = blogTestData;
-  const blogListEmpty = [];
-  const blogListOne = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-  ];
+const blogListEmpty = [];
+const blogListOne = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
 
+describe("total likes", () => {
   test("of empty list is 0", () => {
     const result = listHelper.totalLikes(blogListEmpty);
     expect(result).toBe(0);
@@ -23,5 +22,27 @@ describe("total likes", () => {
   test("when list has only one blog, equals the likes of that", () => {
     const result = listHelper.totalLikes(blogListOne);
     expect(result).toBe(5);
+  });
+
+  test("when list has many blogs, equals the sum of all blogs likes", () => {
+    const result = listHelper.totalLikes(blogTestData);
+    expect(result).toBe(36);
+  });
+});
+
+describe("favorite blog", () => {
+  test("when list has many blogs, returns the blog with the highest like count", () => {
+    const result = listHelper.favoriteBlog(blogTestData);
+    expect(result.likes).toBe(12);
+  });
+
+  test("when list has one blog, returns the only blog", () => {
+    const result = listHelper.favoriteBlog(blogListOne);
+    expect(result.likes).toBe(5);
+  });
+
+  test("when list is empty, returns null", () => {
+    const result = listHelper.favoriteBlog(blogListEmpty);
+    expect(result).toEqual({});
   });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,9 +1,23 @@
-const dummy = (blogs) => 1;
-
 const totalLikes = (blogs) =>
   blogs.reduce((total, blog) => total + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+
+  let mostLikes = 0;
+  let favoriteBlogIndex = 0;
+
+  blogs.forEach((blog, index) => {
+    if (blog.likes > mostLikes) {
+      mostLikes = blog.likes;
+      favoriteBlogIndex = index;
+    }
+  });
+
+  return blogs[favoriteBlogIndex];
+};
+
 module.exports = {
-  dummy,
   totalLikes,
+  favoriteBlog,
 };
