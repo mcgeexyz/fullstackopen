7093288f744ab699c0fbdb3d11cf142130e67b0c
Michael McGee <github@m3t4m1k3.com>
2022-08-27 02:01:29 +0930

    Complete part4 exercise 10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,12 +1,20 @@
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 const app = require("../app");
+const Blog = require("../models/blog");
+const blogData = require("./test_data");
 
 const api = supertest(app);
 
-// const Blog = require("../models/blog");
-
 jest.setTimeout(100000);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = blogData.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
 
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
@@ -25,6 +33,29 @@ test("a blogs unique identifier property is named 'id'", async () => {
   const response = await api.get("/api/blogs");
 
   expect(response.body[0].id).toBeDefined();
+});
+
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "Monkey Coding",
+    author: "Mike Ling",
+    url: "https://monkeycoding.com/",
+    likes: 39,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  // Total number of blogs increased by one
+  const blogsAtEnd = await Blog.find({});
+  expect(blogsAtEnd).toHaveLength(blogData.length + 1);
+
+  // The title of the new blog is in the database
+  const blogTitles = blogsAtEnd.map((blog) => blog.title);
+  expect(blogTitles).toContain("Monkey Coding");
 });
 
 afterAll(() => {
