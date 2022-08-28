95ad0c67cde6073820273950a9f9e9231519eef8
Michael McGee <github@m3t4m1k3.com>
2022-08-28 21:22:42 +0930

    Complete part4

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import noteService from './services/notes';
+import loginService from './services/login';
 import Note from './components/Note';
 import Notification from './components/Notification';
 
@@ -25,6 +26,22 @@ const App = () => {
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
   const [notification, setNotification] = useState({ message: '' });
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      notify('Wrong credentials', 'error');
+    }
+  };
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -66,17 +83,64 @@ const App = () => {
       .then((returnedNote) => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
-      .catch((err) => {
+      .catch(() => {
         setNotes(notes.filter((note) => note.id !== id));
       });
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  );
+
   return (
     <div>
       <h1>Notes</h1>
+
       <Notification notification={notification} />
+
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}
+
+      <h2>Notes</h2>
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -91,13 +155,9 @@ const App = () => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
+
+      {/**/}
+
       <Footer />
     </div>
   );
--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://mcgee-notes-backend.herokuapp.com/api/notes';
+const baseUrl = '/api/notes';
 
 const getAll = () => {
   return axios.get(baseUrl).then((res) => res.data);
